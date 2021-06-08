38636c8e110503176ceac4a29d29b91af36a5005
Marta Santana <dra.martasantana@hotmail.com>
2021-06-08 15:09:50 -0300

    ataulizado

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('covid');
+});
+
 Route::get('/projeto', function () {
     return view('covid');
 });
