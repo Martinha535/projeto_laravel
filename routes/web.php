022235e7cdf52be5079959890051af7994bd5f0b
Marta Santana <dra.martasantana@hotmail.com>
2021-06-14 12:39:21 -0300

    Atualizado

--- routes/web.php
+++ routes/web.php
@@ -23,30 +23,28 @@ Route::get('/projeto', function () {
     return view('covid');
 });
 
-Route::get('teste', function(){
-    Paciente
+// Route::get('teste', function(){
+//     Paciente
+// });
+
+
+Route::get('paciente', array('as' => 'paciente', 'uses' => 'PacienteController@paciente'));
+Route::post('paciente', 'PacienteController@postPaciente');
+
+Route::get('/paciente', 'PacienteController@paciente');
+
+Route::group(['prefix' => 'paciente'], function () {
+    Route::get('/covid', 'PacienteController@covid')->name('paciente.covid');
+    Route::get('/cadastrar', 'PacienteController@cadastrar')->name('paciente.cadastrar');
+    Route::get('/queixa', 'PacienteController@queixa')->name('paciente.queixa');
+    Route::post('/queixa', 'PacienteController@criarQueixa')->name('paciente.queixa.criar');
+    Route::get('/opinar', 'PacienteController@opinar')->name('paciente.opinar');
+    Route::get('/sobre', 'PacienteController@sobre')->name('paciente.sobre');
+    Route::get('/finalizar', 'PacienteController@finalizar')->name('paciente.finalizar');
+    Route::get('/login', 'PacienteController@login')->name('paciente.login');
+    Route::post('/logar', 'loginController@login')->name('login');
 });
-
-
-Route::get('paciente',array('as'=>'paciente', 'uses'=>'PacienteController@paciente'));
-Route::post('paciente','PacienteController@postPaciente');
-
-Route::get('/paciente','PacienteController@paciente');
-
-Route::group(['prefix'=>'paciente'], function ){
-Route::get('/covid','PacienteController@covid')->name('paciente.covid');
-Route::get('/cadastrar','PacienteController@cadastrar')->name('paciente.cadastrar');
-Route::get('/queixa','PacienteController@queixa')->name('paciente.queixa');
-Route::post('/queixa','PacienteController@criarQueixa')->name('paciente.queixa.criar'); 
-Route::get('/opinar','PacienteController@opinar')->name('paciente.opinar');
-Route::get('/sobre','PacienteController@sobre')->name('paciente.sobre');
-Route::get('/finalizar','PacienteController@finalizar')->name('paciente.finalizar');
-Route::get('/login','PacienteController@login')->name('paciente.login');
-Route::post('/logar','loginController@login')->name('login');
 
 Route::middleware(['login'])->group(function () {
-    Route::get('/informar','PacienteController@informar')->name('paciente.informar');
-});
-
-
+    Route::get('/informar', 'PacienteController@informar')->name('paciente.informar');
 });
